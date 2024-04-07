57a31ade963e906558549abf5d65e8eb70131a58
Aayush Shrestha <aayushshres20@gmail.com>
2024-04-07 16:28:10 +0530

    updated create room page ui

--- lib/pages/create_room_page.dart
+++ lib/pages/create_room_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skribbl_app/core/theme/app_pallete.dart';
 import 'package:skribbl_app/pages/waiting_page.dart';
+import 'package:skribbl_app/widgets/dropdown_button.dart';
 import 'package:skribbl_app/widgets/gradient_button.dart';
 import 'package:skribbl_app/widgets/gradient_textfield.dart';
 
@@ -15,6 +16,8 @@ class _CreateRoomPageState extends State<CreateRoomPage> {
   final playerNameController = TextEditingController();
   final roomNameController = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  late String maxRoundValue;
+  late String numberOfPlayers;
 
   @override
   void dispose() {
@@ -29,42 +32,55 @@ class _CreateRoomPageState extends State<CreateRoomPage> {
         appBar: AppBar(
           iconTheme: const IconThemeData(color: AppPallete.gradient1),
         ),
-        body: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Create Room",
-                  style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: AppPallete.gradient1),
-                ),
-                const SizedBox(height: 30),
-                GradientTextField(
-                  hintText: 'Enter your name',
-                  controller: playerNameController,
-                ),
-                const SizedBox(height: 15),
-                GradientTextField(
-                  hintText: 'Enter room name',
-                  controller: roomNameController,
-                ),
-                const SizedBox(height: 15),
-                GradientButton(
-                    buttonText: "Create",
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const WaitingPage(),
-                          ),
-                        );
-                      }
-                    })
-              ],
-            )));
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Create Room",
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: AppPallete.gradient1),
+                  ),
+                  const SizedBox(height: 30),
+                  GradientTextField(
+                    hintText: 'Enter your name',
+                    controller: playerNameController,
+                  ),
+                  const SizedBox(height: 15),
+                  GradientTextField(
+                    hintText: 'Enter room name',
+                    controller: roomNameController,
+                  ),
+                  const SizedBox(height: 15),
+                  const DropdownWidget(
+                    hintText: "Max Rounds",
+                    itemList: ["3", "5", "7", "9"],
+                  ),
+                  const SizedBox(height: 15),
+                  const DropdownWidget(
+                    hintText: "Number of Players",
+                    itemList: ["2", "3", "4", "5"],
+                  ),
+                  const SizedBox(height: 20),
+                  GradientButton(
+                      buttonText: "Create",
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const WaitingPage(),
+                            ),
+                          );
+                        }
+                      })
+                ],
+              )),
+        ));
   }
 }
