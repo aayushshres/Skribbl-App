525b946cd66523c801e94f0ab2b6f580b0b44657
Aayush Shrestha <aayushshres20@gmail.com>
2024-04-15 21:45:11 +0530

    socket connection

--- lib/pages/paint_screen.dart
+++ lib/pages/paint_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class PaintScreen extends StatefulWidget {
   final Map<String, String> data;
@@ -14,8 +15,32 @@ class PaintScreen extends StatefulWidget {
 }
 
 class _PaintScreenState extends State<PaintScreen> {
+  late io.Socket _socket;
+
+  @override
+  void initState() {
+    connect();
+    super.initState();
+  }
+
+  // socket io client connection
+  void connect() {
+    _socket = io.io("http://localhost:3000", <String, dynamic>{
+      'transports': ['websocket'],
+      'autoconnect': false
+    });
+    _socket.connect();
+
+    // listen to socket
+    _socket.onConnect((data) {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Paint Screen"),
+      ),
+    );
   }
 }
